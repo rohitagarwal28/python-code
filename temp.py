1c6f643aaf4a0b100d229596fb4be220ed4691cc
rohit agarwal <rohitagarwal516@gmail.com>
2020-05-26 21:57:08 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,7 @@
+import rohit1
+principal=int(input("enter the principal"))
+rate=int(input("enter the rate"))
+time=int(input("enter the time"))
+rohit1.simple_interest(principal,rate,time)
+li=list(int(input("enter the list:")))
+rohit1.avg(li)
